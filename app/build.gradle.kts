710713dbe825e30134f2b79bd5419766b0a4f991
YuriyPetukhov <pochtalosyahs@gmail.com>
2025-04-10 13:52:59 +0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+import org.gradle.kotlin.dsl.implementation
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp") version "2.1.20-1.0.32"
+}
+
+android {
+    namespace = "com.example.mycryptoapp"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.mycryptoapp"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+    val room_version = "2.6.1"
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.androidx.room.runtime)
+    implementation (libs.adapter.rxjava2)
+    implementation (libs.converter.gson)
+    implementation (libs.rxandroid)
+    implementation (libs.rxjava)
+    implementation(libs.dagger.compiler)
+    ksp(libs.dagger.compiler)
+    //ViewModel
+    implementation(libs.viewModel)
+//Room
+    implementation(libs.room)
+// Кодогенератор Room
+    ksp(libs.room.compiler)
+// optional - Kotlin Extensions and Coroutines support for Room
+    implementation(libs.room.ktx)
+}
---
+++ app/src/main/java/com/example/mycryptoapp/domain/GetCryptoCoinByName.kt
@@ -0,0 +1,4 @@
+package com.example.mycryptoapp.domain
+
+class GetCryptoCoinByName {
+}
